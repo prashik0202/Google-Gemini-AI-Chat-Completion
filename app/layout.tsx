b06f1be921a89d2f3444820119e00369ab5298b2
Prashik <gamreprashik03@gmail.com>
2024-10-13 07:19:58 +0530

    add sense

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8106330110392557"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
